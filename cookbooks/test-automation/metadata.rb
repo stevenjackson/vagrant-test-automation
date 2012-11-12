69b1203589cfdaa5a34656c3000c1b03e7e64b81
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-11-12 07:58:53 -0500

    Created test-automation recipe

---
+++ cookbooks/test-automation/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Steve Jackson"
+maintainer_email "steve.jackson@leandogsoftware.com"
+license          "Apache 2.0"
+description      "Installs/Configures packages for cucumber testing"
+
+version          "0.1"
